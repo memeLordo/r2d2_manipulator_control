dcab3a442cd286c38fb3d13eb71d045c29ba1ae1
memeLordo <serafim.kripachev@gmail.com>
2025-08-13 17:39:40 +0300

    Обновление реализации функции полинома

--- include/manipulator_control/HornerPolynome.h
+++ include/manipulator_control/HornerPolynome.h
@@ -1,4 +1,20 @@
-#include <vector>
+#ifndef POLYNOME_H
+#define POLYNOME_H
 
-// TODO: add template
-double h_polynome(const std::vector<double> &coeff, double x);
+#include <cstddef>
+
+namespace Horner {
+
+template <typename T, size_t N>
+constexpr T polynome(const T (&coeffs)[N], T x) {
+  // static_assert(N > 0, "Coefficient array cannot be empty");
+  if (N == 0)
+    return 0;
+  T result = coeffs[N - 1];
+  for (size_t i = N - 1; i > 0; --i)
+    result = result * x + coeffs[i - 1];
+  return result;
+}
+
+} // namespace Horner
+#endif // POLYNOME_SAFE_H
