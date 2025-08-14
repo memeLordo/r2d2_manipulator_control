d2089974c954142d5277221cc222ac7dac45403f
memeLordo <serafim.kripachev@gmail.com>
2025-08-14 11:53:27 +0300

    Добавление файла реализации ManipulatorControlHandler

---
+++ src/ManipulatorControl.cpp
@@ -0,0 +1,15 @@
+#include "ManipulatorControlHandler.h"
+#include <ros/ros.h>
+#include <std_msgs/Int64.h>
+
+ManipulatorControlHandler::ManipulatorControlHandler(ros::NodeHandle *node)
+    : elbowHandler(node), shoulderHandler(node) {
+  subscriber =
+      node->subscribe("/manupulator/payload_input", 1000,
+                      &ManipulatorControlHandler::callback_manipulator, this);
+  publisher =
+      node->advertise<std_msgs::Int64>("/manipulator/payload_output", 10);
+}
+
+void ManipulatorControlHandler::callback_manipulator(
+    const std_msgs::Int64 &msg) {}
