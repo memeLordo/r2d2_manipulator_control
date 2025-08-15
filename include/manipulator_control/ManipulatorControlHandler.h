aca8d2d9b9001725082cf78d0b2a38078677ac7e
memeLordo <serafim.kripachev@gmail.com>
2025-08-15 17:26:15 +0300

    Добавление PayloadHandler

--- include/manipulator_control/ManipulatorControlHandler.h
+++ include/manipulator_control/ManipulatorControlHandler.h
@@ -2,22 +2,23 @@
 #define MANIPULATOR_CONTROL_HANDLER_H
 
 #include "ElbowHandler.h"
+#include "PayloadHandler.h"
 #include "ShoulderHandler.h"
-#include "r2d2_msg_pkg/DriverState.h"
 #include <ros/ros.h>
 
 class ManipulatorControlHandler {
 private:
   ElbowHandler elbow;
   ShoulderHandler shoulder;
+  PayloadHandler payload;
 
   ros::Subscriber subscriber;
   ros::Publisher publisher;
 
 public:
   ManipulatorControlHandler(ros::NodeHandle *node);
-  void callback_manipulator(const r2d2_msg_pkg::DriverStateConstPtr &msg);
   void setup();
+  void callback_manipulator();
 };
 
 #endif // MANIPULATOR_CONTROL_HANDLER_H
