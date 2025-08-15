30552d9b1506b5272889b2b34293248e1eb28d70
memeLordo <serafim.kripachev@gmail.com>
2025-08-15 21:11:16 +0300

    Обновление совместимости аргументов polynome()

--- include/manipulator_control/Polynome.h
+++ include/manipulator_control/Polynome.h
@@ -6,24 +6,28 @@
 
 namespace Horner {
 
-template <typename T, size_t N>
-constexpr T polynome(const T (&coeffs)[N], T x) {
+template <typename T, typename T2, size_t N>
+constexpr T polynome(const T (&coeffs)[N], T2 x) {
   // static_assert(N > 0, "Coefficient array cannot be empty");
   if (N == 0)
     return T{0};
+
+  T _x = static_cast<T>(x);
   T result = coeffs[N - 1];
   for (size_t i = N - 1; i > 0; --i)
-    result = result * x + coeffs[i - 1];
+    result = result * _x + coeffs[i - 1];
   return result;
 }
 
-template <typename T> T polynome(const std::vector<T> &coeffs, T x) {
+template <typename T, typename T2>
+T polynome(const std::vector<T> &coeffs, T2 x) {
   if (coeffs.empty())
     return T{0};
 
+  T _x = static_cast<T>(x);
   T result = coeffs.back();
   for (size_t i = coeffs.size() - 1; i > 0; --i)
-    result = result * x + coeffs[i - 1];
+    result = result * _x + coeffs[i - 1];
   return result;
 }
 
