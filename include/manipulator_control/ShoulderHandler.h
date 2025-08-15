ff3abd933bc9a92c3f77287be7383234d32fce19
memeLordo <serafim.kripachev@gmail.com>
2025-08-15 21:12:32 +0300

    Реализация функций взаимодействия с переменными нод

--- include/manipulator_control/ShoulderHandler.h
+++ include/manipulator_control/ShoulderHandler.h
@@ -2,13 +2,16 @@
 #define SHOULDER_HANDLER_H
 
 #include "PipeHandler.h"
+#include "Polynome.h"
 #include "r2d2_msg_pkg/DriverState.h"
 #include <cstdint>
 #include <ros/ros.h>
 
+#define MAX_SIZE 3
+
 class ShoulderHandler {
 private:
-  static const double coeffs[];
+  static const double coeffs[MAX_SIZE];
 
   struct shoulder_t {
     int16_t omega{};
@@ -26,13 +29,26 @@ public:
   void callback_shoulder(const r2d2_msg_pkg::DriverStateConstPtr &msg) {
     callback_params = shoulder_t{msg->omega, msg->theta};
   };
-  // void get_input_speed(const r2d2_msg_pkg::DriverCommandConstPtr &msg);
-  // void get_input_angle(const r2d2_msg_pkg::DriverCommandConstPtr &msg);
-  void update_speed(const double *omega = nullptr);
-  void update_angle(const double *theta = nullptr);
-  double get_speed();
-  double get_angle();
-  double calc_angle(const double *theta = nullptr);
+  void update_speed() { params.omega = callback_params.omega; };
+  void update_angle() { params.theta = callback_params.theta; };
+  template <typename T> void update_speed(T omega) {
+    params.omega = static_cast<int16_t>(omega);
+  };
+  template <typename T> void update_angle(T theta) {
+    params.theta = static_cast<int16_t>(theta);
+  };
+  template <typename T = double> const T get_speed() const {
+    return static_cast<T>(params.omega);
+  };
+  template <typename T = double> const T get_angle() const {
+    return static_cast<T>(params.theta);
+  };
+  template <typename T = double> constexpr T calc_angle() {
+    return Horner::polynome(coeffs, pipe.get_radius());
+  };
+  template <typename T = double> constexpr T calc_angle(T theta) {
+    return Horner::polynome(coeffs, theta);
+  }
 };
 
 #endif // SHOULDER_HANDLER_H
