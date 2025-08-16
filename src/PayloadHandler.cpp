eaed743c000951cc0914ed9d86b3702c77bff90f
memeLordo <serafim.kripachev@gmail.com>
2025-08-16 18:12:31 +0300

    Переименование стартовых топиков

--- src/PayloadHandler.cpp
+++ src/PayloadHandler.cpp
@@ -1,7 +1,7 @@
 #include "PayloadHandler.h"
 #include <ros/ros.h>
 
-#define PAYLOAD_OUTPUT_NODE "/manupulator/payload_output"
+#define PAYLOAD_OUTPUT_NODE "/payload_output"
 
 PayloadHandler::PayloadHandler(ros::NodeHandle *node) {
   subscriber = node->subscribe(PAYLOAD_OUTPUT_NODE, 1000,
