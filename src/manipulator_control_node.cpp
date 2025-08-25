1510c4eca188321ca9b3671a53999adb92f2c17d
memeLordo <serafim.kripachev@gmail.com>
2025-08-25 23:56:54 +0300

    Добавление асинхронности
    Это решило проблему с замыканием клиента в
    ManipulatorControlHandler.cpp:14:7.

--- src/manipulator_control_node.cpp
+++ src/manipulator_control_node.cpp
@@ -5,7 +5,10 @@
 int main(int argc, char **argv) {
   ros::init(argc, argv, "manipulator_control");
   ros::NodeHandle node;
+
+  ros::AsyncSpinner spinner(2);
+  spinner.start();
   ManipulatorServiceHandler ms(&node);
   ManipulatorControlHandler<> mc(&node);
-  ros::spin();
+  ros::waitForShutdown();
 }
