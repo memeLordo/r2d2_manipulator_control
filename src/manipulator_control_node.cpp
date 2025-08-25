9388a919c03bd448c16e9541bbe71bbfa89f229b
memeLordo <serafim.kripachev@gmail.com>
2025-08-25 20:55:42 +0300

    Инициализация ManipulatorServiceHandler

--- src/manipulator_control_node.cpp
+++ src/manipulator_control_node.cpp
@@ -1,9 +1,11 @@
 #include "ManipulatorControl.h"
+#include "ManipulatorService.h"
 #include <ros/node_handle.h>
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "manipulator_control");
-  ros::NodeHandle node{};
+  ros::NodeHandle node;
+  ManipulatorServiceHandler ms(&node);
   ManipulatorControlHandler<> mc(&node);
   ros::spin();
 }
