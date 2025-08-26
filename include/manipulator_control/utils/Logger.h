0cb6fa389d174985088d913288e387119c1ce0e0
memeLordo <serafim.kripachev@gmail.com>
2025-08-26 18:00:15 +0300

    Добавление debug-логирования

---
+++ include/manipulator_control/utils/Logger.h
@@ -0,0 +1,12 @@
+#ifndef PR_MANIPULATOR_CONTROL_DEBUG
+#include <ros/console.h>
+#if DEBUG == 1
+#define LOG_INFO(x) ROS_INFO(x)
+#define LOG_WARN(x) ROS_WARN(x)
+#define LOG_ERROR(x) ROS_ERROR(x)
+#else
+#define LOG_INFO(x)
+#define LOG_WARN(x)
+#define LOG_ERROR(x)
+#endif
+#endif // !PR_MANIPULATOR_CONTROL_DEBUG
