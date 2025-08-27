544ad917b27e004d5162e6a974294390c6a53de5
memeLordo <serafim.kripachev@gmail.com>
2025-08-27 12:24:14 +0300

    Удаление явной реализации через функции
    Есть более универсальный метод: через launch-файл.

--- include/manipulator_control/utils/Debug.h
+++ include/manipulator_control/utils/Debug.h
@@ -3,25 +3,5 @@
 #include <ros/node_handle.h>
 
 
-namespace r2d2_debug {
 
-inline void enableLogging() {
-  ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME,
-                                 ros::console::levels::Debug);
-  ros::console::notifyLoggerLevelsChanged();
-}
-
-inline void checkLogging(const ros::NodeHandle *node) {
-  if (!node->hasParam("logger_level")) {
-    ROS_WARN("Logger level not set");
-    return;
-  }
-  std::string level{};
-  node->param("logger_level", level);
-  if (level != "DEBUG") {
-    enableLogging();
-  }
-}
-
-} // namespace r2d2_debug
 #endif // !PR_MANIPULATOR_CONTROL_DEBUG
--- src/manipulator_control_node.cpp
+++ src/manipulator_control_node.cpp
@@ -1,12 +1,10 @@
 #include "ManipulatorControl.h"
 #include "ManipulatorService.h"
-#include "utils/Debug.h"
 #include <ros/node_handle.h>
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "manipulator_control");
   ros::NodeHandle node;
-  r2d2_debug::checkLogging(&node);
   // ros::AsyncSpinner spinner(2);
   // spinner.start();
   ManipulatorControlHandler<> mc(&node);
