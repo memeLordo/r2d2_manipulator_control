0093a68e43215ba218a871542273f5838b5e57c3
memeLordo <serafim.kripachev@gmail.com>
2025-08-29 00:27:42 +0300

    Объявление r2d2_commands
    С определением типа ControlType.

--- include/manipulator_control/utils/Types.h
+++ include/manipulator_control/utils/Types.h
@@ -1,5 +1,15 @@
 #ifndef CONTROL_WORD_H
 #define CONTROL_WORD_H
 
+namespace r2d2_commands {
+
+enum class ControlType : uint16_t {
+  HOLD = 0x00,
+  CONTROL_SPEED = 0x07,
+  CONTROL_ANGLE = 0x0A,
+  CHECK_ID = 0x800
+};
+
+} // namespace r2d2_commands
 
 #endif // CONTROL_WORD_H
