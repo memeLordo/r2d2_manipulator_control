d01ef77ea3f3954eaf27b40a917fcbcf5a763cc3
memeLordo <serafim.kripachev@gmail.com>
2025-08-31 22:43:05 +0300

    Создание CustomDebug.h
    С первычной импементацией логирования функцй для ROS.

---
+++ include/manipulator_control/utils/CustomDebug.h
@@ -0,0 +1,21 @@
+#ifndef CUSTOM_ROS_DEBUG_H
+#define CUSTOM_ROS_DEBUG_H
+
+#include "Debug.h"
+#include <ros/console.h>
+
+// // ROS debug function (requires ROS environment)
+// #define ROS_DEBUG_FUNC(output, ...) \
+//   do { \
+//     std::ostringstream oss; \
+//     oss << MAGENTA(__func__); \
+//     debug_print_args(oss, #__VA_ARGS__, __VA_ARGS__); \
+//     oss << " : " << WHITE(output); \
+//     ROS_DEBUG_STREAM(oss.str()); \
+//   } while (0)
+
+#define ROS_DEBUG_FUNC(output, ...)                                            \
+  _LOG_FUNC([](const std::string &msg) { ROS_DEBUG_STREAM(msg) }, output,      \
+            __VA_ARGS__)
+
+#endif // !PR_ROS_DEBUG_H
