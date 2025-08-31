4583d1ee24820282d180828887fed845d5db4947
memeLordo <serafim.kripachev@gmail.com>
2025-08-31 18:07:55 +0300

    Config.h: оптимизация типов
    Под std::vector вместо std::array.

--- include/manipulator_control/utils/Config.h
+++ include/manipulator_control/utils/Config.h
@@ -1,15 +1,15 @@
 #ifndef R2D2_CONFIG_H
 #define R2D2_CONFIG_H
 
-#include <array>
 #include <string>
+#include <vector>
 namespace config {
 
 namespace shoulder {
 const std::string INPUT_NODE = "/shoulder_input";
 const std::string OUTPUT_NODE = "/shoulder_output";
 
-const std::array<double, 3> coeffs{0.00024, -0.142, 20.9};
+const std::vector<double> coeffs{0.00024, -0.142, 20.9};
 const int length{363}; // mm
 const int speed{100};
 // const double angle_treshold{5};
@@ -19,14 +19,14 @@ namespace elbow {
 const std::string INPUT_NODE = "/elbow_input";
 const std::string OUTPUT_NODE = "/elbow_output";
 
-const std::array<double, 3> coeffs{-0.00011, 0.341, -105.2};
+const std::vector<double> coeffs{-0.00011, 0.341, -105.2};
 const int length{180}; // mm
 const int speed{100};
 // const double angle_treshold{5};
 } // namespace elbow
 
 namespace pipe {
-const std::string OUTPUT_NODE = "/pipe_output";
+const std::string OUTPUT_NODE = "/parameters/pipe";
 }
 
 namespace payload {
