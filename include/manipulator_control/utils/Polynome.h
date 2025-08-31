d244089c1a6745891328e273a059dbcbbdc6347f
memeLordo <serafim.kripachev@gmail.com>
2025-08-31 18:05:39 +0300

    Обновление polynome
    Интепритация для ввода значений сначала до конца:
    m_coeffs{4, 3, 2, 1} -> f(x) = 4x^3 + 3x^2 + 2x + 1

--- include/manipulator_control/utils/Polynome.h
+++ include/manipulator_control/utils/Polynome.h
@@ -7,14 +7,14 @@
 namespace horner {
 
 template <typename T, typename T2>
-T polynome(const std::vector<T> &coeffs, T2 x) {
+T polynome(const std::vector<T> &coeffs, const T2 x) {
   if (coeffs.empty())
     return T{0};
-  T _x = static_cast<T>(x);
-  T result = coeffs.back();
-  for (size_t i = coeffs.size() - 1; i > 0; --i)
-    result = result * _x + coeffs[i - 1];
-  return result;
+  const T x_ = static_cast<T>(x);
+  T result_ = coeffs[0];
+  for (size_t i = 1; i < coeffs.size(); ++i)
+    result_ = result_ * x_ + coeffs[i];
+  return result_;
 }
 
 } // namespace horner
