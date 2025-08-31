5bdf003bdfa9c8ae2e1528872691ff9a05c68aa0
memeLordo <serafim.kripachev@gmail.com>
2025-08-31 17:10:36 +0300

    Переход polynome: C-array-> std::array
    Для реализации работы с обновлёнными std::array-типами.

--- include/manipulator_control/utils/Polynome.h
+++ include/manipulator_control/utils/Polynome.h
@@ -1,13 +1,13 @@
 #ifndef POLYNOME_H
 #define POLYNOME_H
 
+#include <array>
 #include <cstddef>
-// #include <vector>
 
 namespace horner {
 
-template <typename T, typename T2, size_t N>
-constexpr T polynome(const T (&coeffs)[N], T2 x) {
+template <typename T, size_t N, typename T2>
+constexpr T polynome(const std::array<T, N> &coeffs, T2 x) {
 
   if (N == 0)
     return T{0};
@@ -18,17 +18,6 @@ constexpr T polynome(const T (&coeffs)[N], T2 x) {
     result = result * x_ + coeffs[i - 1];
   return result;
 }
-
-// template <typename T, typename T2>
-// T polynome(const std::vector<T> &coeffs, T2 x) {
-//   if (coeffs.empty())
-//     return T{0};
-//   T _x = static_cast<T>(x);
-//   T result = coeffs.back();
-//   for (size_t i = coeffs.size() - 1; i > 0; --i)
-//     result = result * _x + coeffs[i - 1];
-//   return result;
-// }
 
 } // namespace horner
 #endif // POLYNOME_SAFE_H
