df45606a57bb52af844d2d5b3cd3477f0d85a573
memeLordo <serafim.kripachev@gmail.com>
2025-08-31 18:16:13 +0300

    Polynome.h: Добавление polynome() для единого типа

--- include/manipulator_control/utils/Polynome.h
+++ include/manipulator_control/utils/Polynome.h
@@ -6,6 +6,16 @@
 
 namespace horner {
 
+template <typename T>
+constexpr T polynome(const std::vector<T> &coeffs, const T x) {
+  if (coeffs.empty())
+    return T{0};
+  T result_ = coeffs[0];
+  for (size_t i = 1; i < coeffs.size(); ++i)
+    result_ = result_ * x + coeffs[i];
+  return result_;
+}
+
 template <typename T, typename T2>
 constexpr T polynome(const std::vector<T> &coeffs, const T2 x) {
   if (coeffs.empty())
