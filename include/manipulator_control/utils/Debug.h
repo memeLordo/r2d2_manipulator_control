6a7b7b2afbe9481c5f56f84abb0bf87be1d15c2f
memeLordo <serafim.kripachev@gmail.com>
2025-09-01 14:56:51 +0300

    Debug.h: Добавление реализации макросов
    __CLASS_NAME__ : "Class",
    __METHOD_NAME__ : "Class::method()"

--- include/manipulator_control/utils/Debug.h
+++ include/manipulator_control/utils/Debug.h
@@ -25,6 +25,32 @@
 #define CYAN(x) ANSI_COLOR_CYAN << x << ANSI_COLOR_RESET
 #define WHITE(x) ANSI_COLOR_WHITE << x << ANSI_COLOR_RESET
 
+// Вспомогательная печать одной пары имя-значение
+inline std::string className(const std::string &prettyFunction) {
+  size_t colons = prettyFunction.find("::");
+  if (colons == std::string::npos)
+    return "::";
+  size_t begin = prettyFunction.substr(0, colons).rfind(" ") + 1;
+  size_t end = colons - begin;
+
+  return prettyFunction.substr(begin, end);
+}
+inline std::string methodName(const std::string &prettyFunction) {
+  size_t colons = prettyFunction.find("::");
+  size_t begin = prettyFunction.substr(0, colons).rfind(" ") + 1;
+  size_t end = prettyFunction.rfind("(") - begin;
+
+  return prettyFunction.substr(begin, end);
+}
+
+// inline std::string prettyName(const std::string &prettyFunction) {
+//   return prettyFunction.substr();
+// }
+
+#define __CLASS_NAME__ className(__PRETTY_FUNCTION__)
+#define __METHOD_NAME__ methodName(__PRETTY_FUNCTION__)
+// #define __FUNC__ prettyName(__PRETTY_FUNCTION__)
+
 inline bool is_valid_var_name(const std::string &name) {
   if (name.empty())
     return false;
