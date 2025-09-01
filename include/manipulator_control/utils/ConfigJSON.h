a2f63f8731e5e7e383e4c7b0a037314ce3577087
memeLordo <serafim.kripachev@gmail.com>
2025-09-01 22:27:25 +0300

    Инициализация ConfigJSON.h

---
+++ include/manipulator_control/utils/ConfigJSON.h
@@ -0,0 +1,12 @@
+#ifndef R2D2_CONFIG_JSON_H
+#define R2D2_CONFIG_JSON_H
+
+#include <nlohmann/json.hpp>
+
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+#endif // R2D2_CONFIG_JSON_H
