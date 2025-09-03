ad79760bb5d4c91854a0e940682c5a5999b53dc4
memeLordo <serafim.kripachev@gmail.com>
2025-09-03 12:25:32 +0300

    Обновление ConfigJson.h

--- include/manipulator_control/utils/ConfigJSON.h
+++ include/manipulator_control/utils/ConfigJSON.h
@@ -3,10 +3,16 @@
 
 #include <nlohmann/json.hpp>
 
-#include <fstream>
-#include <iostream>
-#include <nlohmann/json.hpp>
+template <typename T> class ConfigJSON {
+  using Json = nlohmann::json;
 
-using json = nlohmann::json;
+private:
+  static Json m_json;
+
+public:
+  ConfigJSON(const std::string &path);
+  static Json s_get() { return m_json; }
+  static void s_parse();
+};
 
 #endif // R2D2_CONFIG_JSON_H
