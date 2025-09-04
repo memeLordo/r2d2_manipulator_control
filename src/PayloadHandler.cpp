59f44636165ca8d1a50b2984b55598afce3eff5c
memeLordo <serafim.kripachev@gmail.com>
2025-09-04 14:33:53 +0300

    Обновление стандартных наименований Pipe и Payload
    См.: "Обновление стандартных наименований Joint-классов"

--- src/PayloadHandler.cpp
+++ src/PayloadHandler.cpp
@@ -1,10 +1,9 @@
 #include "PayloadHandler.hpp"
-#include "utils/Config.hpp"
 #include <ros/node_handle.h>
 
 template <typename T>
 PayloadHandler<T>::PayloadHandler(ros::NodeHandle *node)
-    : m_outputNode{config::payload::OUTPUT_NODE} {
+    : m_outputNode{"/payload_output"} {
   constexpr int QUEUE_SIZE = 8;
   m_subscriber = node->subscribe(m_outputNode, QUEUE_SIZE,
                                  &PayloadHandler::callbackPayload, this);
--- src/PipeHandler.cpp
+++ src/PipeHandler.cpp
@@ -1,10 +1,9 @@
 #include "PipeHandler.hpp"
-#include "utils/Config.hpp"
 #include <ros/node_handle.h>
 
 template <typename T>
 PipeHandler<T>::PipeHandler(ros::NodeHandle *node)
-    : m_outputNode{config::pipe::OUTPUT_NODE} {
+    : m_outputNode{"/parameters/pipe"} {
   constexpr int QUEUE_SIZE = 8;
   m_subscriber = node->subscribe(m_outputNode, QUEUE_SIZE,
                                  &PipeHandler::callbackPipe, this);
