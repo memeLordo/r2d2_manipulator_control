34eeffaab2baa3d6efc636b9e9d935445313298e
memeLordo <serafim.kripachev@gmail.com>
2025-09-05 00:21:05 +0300

    fixup! JointHandler: Обновление имён логирования
    Для дальнейшей возможной реализации через Json-файл.

--- include/manipulator_control/utils/Config.hpp
+++ include/manipulator_control/utils/Config.hpp
@@ -1,13 +1,10 @@
 #ifndef R2D2_CONFIG_HPP
 #define R2D2_CONFIG_HPP
 
-#include <string>
 #include <vector>
 namespace config {
 
 namespace shoulder {
-const std::string INPUT_NODE = "/shoulder_input";
-const std::string OUTPUT_NODE = "/shoulder_output";
 
 const std::vector<double> coeffs{0.00024, -0.142, 20.9};
 const int length{363}; // mm
@@ -16,22 +13,12 @@ const int speed{100};
 } // namespace shoulder
 
 namespace elbow {
-const std::string INPUT_NODE = "/elbow_input";
-const std::string OUTPUT_NODE = "/elbow_output";
 
 const std::vector<double> coeffs{-0.00011, 0.341, -105.2};
 const int length{180}; // mm
 const int speed{100};
 // const double angle_treshold{5};
 } // namespace elbow
-
-namespace pipe {
-const std::string OUTPUT_NODE = "/parameters/pipe";
-}
-
-namespace payload {
-const std::string OUTPUT_NODE = "/payload_output";
-}
 
 } // namespace config
 
