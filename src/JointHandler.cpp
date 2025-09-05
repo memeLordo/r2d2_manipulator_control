f7cf3c666c40b1e1fe53c1b5a1484cd784616c2b
memeLordo <serafim.kripachev@gmail.com>
2025-09-06 02:20:12 +0300

    fixup! Добавление параметров offset/tolerance

--- src/JointHandler.cpp
+++ src/JointHandler.cpp
@@ -7,11 +7,11 @@ using namespace config;
 
 template <typename T>
 JointHandler<T>::JointHandler(ros::NodeHandle *node, const std::string &name,
-                              const std::string &inputTopic,
-                              const std::string &outputTopic, const T &length,
+                              const std::string &input,
+                              const std::string &output, const T &length,
                               const T &speed, const T &offset,
                               const T &tolerance, const std::vector<T> &coeffs)
-    : m_name{name}, m_inputTopic{inputTopic}, m_outputTopic{outputTopic},
+    : m_name{name}, m_inputTopic{input}, m_outputTopic{output},
       m_length{length}, m_speed{speed}, m_angleOffset{offset},
       m_angleTolerance{tolerance}, m_coeffs{coeffs} {
   waitForTopic();
@@ -29,13 +29,13 @@ template <typename T> T JointHandler<T>::calcAngle(T radius) {
 template <typename T>
 ShoulderHandler<T>::ShoulderHandler(ros::NodeHandle *node)
     : JointHandler<T>(node, "Shoulder", "/shoulder_input", "/shoulder_output",
-                      shoulder::length, shoulder::speed, shoulder::offset,
-                      shoulder::tolerance, shoulder::coeffs) {}
+                      shoulder::length, shoulder::speed, shoulder::angle_offset,
+                      shoulder::angle_tolerance, shoulder::coeffs) {}
 template <typename T>
 ElbowHandler<T>::ElbowHandler(ros::NodeHandle *node)
     : JointHandler<T>(node, "Elbow", "/elbow_input", "/elbow_output",
-                      elbow::length, elbow::speed, elbow::offset,
-                      elbow::tolerance, elbow::coeffs) {}
+                      elbow::length, elbow::speed, elbow::angle_offset,
+                      elbow::angle_tolerance, elbow::coeffs) {}
 
 template class JointHandler<>;
 template class ShoulderHandler<>;
