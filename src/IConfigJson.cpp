71dcec59222382600b9a529915f2055d3100a9e3
memeLordo <serafim.kripachev@gmail.com>
2025-09-08 23:44:29 +0300

    Реализация IConfigJson.cpp

--- src/IConfigJson.cpp
+++ src/IConfigJson.cpp
@@ -1,3 +1,20 @@
 #include "utils/IConfigJson.hpp"
+#include "utils/Debug.hpp"
+#include <fstream>
+#include <ros/package.h>
 
-template <typename T> IConfigJson<T>::IConfigJson(const std::string &name) {}
+template <typename T>
+IConfigJson<T>::IConfigJson(const std::string &name)
+    : m_path{ros::package::getPath("manipulator_control") + "/" + m_dirname +
+             "/"} {
+  try {
+    std::ifstream file(m_path + lower(name) + ".json");
+    if (!file)
+      throw std::runtime_error("Cannot open config file");
+    file >> m_json;
+  } catch (const std::exception &e) {
+    ROS_ERROR_STREAM(RED(e.what()));
+    m_json = Json::object();
+  }
+}
+template class IConfigJson<>;
