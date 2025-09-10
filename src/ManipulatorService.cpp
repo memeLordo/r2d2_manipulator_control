9dff7ffc139ce4401824f27057cd9203ab6041e6
memeLordo <serafim.kripachev@gmail.com>
2025-09-10 15:32:18 +0300

    fixup! r2d2_state: Добавление метода toString() в EnupPair

--- src/ManipulatorService.cpp
+++ src/ManipulatorService.cpp
@@ -14,49 +14,23 @@ bool ManipulatorServiceHandler::callbackModeService(
     ManipulatorCommand::Request &req, ManipulatorCommand::Response &res) {
   ROS_DEBUG_STREAM(
       "callbackModeService::got request, work_mode: " << req.work_mode);
-  auto work_mode_ = static_cast<WorkMode>(req.work_mode);
-  switch (work_mode_) {
-  case WorkMode::MANUAL:
-  case WorkMode::AUTO:
-  case WorkMode::STOP:
-    m_manipulatorControl->setMode(work_mode_);
-    break;
-  default:
-    ROS_ERROR_STREAM(("Got unknown work mode"));
+  if (!m_manipulatorControl->setMode(req.work_mode))
     res.success &= false;
-  }
   return true;
 }
 bool ManipulatorServiceHandler::callbackNozzleService(
     ManipulatorCommand::Request &req, ManipulatorCommand::Response &res) {
   ROS_DEBUG_STREAM(
       "callback_nozzle_service::got request, nozzle_type: " << req.nozzle_type);
-  auto nozzle_type_ = static_cast<NozzleType>(req.nozzle_type);
-  switch (nozzle_type_) {
-  case NozzleType::BRUSH:
-  case NozzleType::EMA:
-    m_manipulatorControl->setNozzle(nozzle_type_);
-    m_manipulatorControl->updateNozzleType();
-    break;
-  default:
-    ROS_ERROR("Got unknown nozzle type");
+  if (!m_manipulatorControl->setNozzle(req.nozzle_type))
     res.success &= false;
-  }
   return true;
 }
 bool ManipulatorServiceHandler::callbackStatusService(
     ManipulatorCommand::Request &req, ManipulatorCommand::Response &res) {
   ROS_DEBUG_STREAM(
       "callback_status_service::got request, lock_status: " << req.lock_status);
-  auto lock_status_ = static_cast<LockStatus>(req.lock_status);
-  switch (lock_status_) {
-  case LockStatus::LOCKED:
-  case LockStatus::UNLOCKED:
-    m_manipulatorControl->setLock(lock_status_);
-    break;
-  default:
-    ROS_ERROR("Got unknown lock status");
+  if (!m_manipulatorControl->setLock(req.lock_status))
     res.success &= false;
-  }
   return true;
 }
