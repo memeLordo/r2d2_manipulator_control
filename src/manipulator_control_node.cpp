1afabd24d1890fd62a4f9b19d33779f74d062d00
memeLordo <serafim.kripachev@gmail.com>
2025-09-10 20:13:29 +0300

    Добавление обработки ошибкок

--- src/manipulator_control_node.cpp
+++ src/manipulator_control_node.cpp
@@ -4,11 +4,15 @@
 int main(int argc, char **argv) {
   ros::init(argc, argv, "manipulator_control");
   ros::NodeHandle node;
-  // ros::AsyncSpinner spinner(2);
-  // spinner.start();
-  ManipulatorControlHandler<> mc(&node);
-  ManipulatorServiceHandler ms(&node, &mc);
-  ROS_INFO("Manipulator Control started");
-  ros::spin();
-  // ros::waitForShutdown();
+  try {
+    // ros::AsyncSpinner spinner(2);
+    // spinner.start();
+    ManipulatorControlHandler<> mc(&node);
+    ManipulatorServiceHandler ms(&node, &mc);
+    ROS_INFO("Manipulator Control started");
+    ros::spin();
+    // ros::waitForShutdown();
+  } catch (std::exception &e) {
+    ROS_ERROR_STREAM("Got exception: " << e.what());
+  }
 }
