7fbb8404a2559875fd6bf9d055e17bfaf097f5f3
memeLordo <serafim.kripachev@gmail.com>
2025-09-11 00:09:22 +0300

    ManipulatorService: Перенос определения в заголовок

--- include/manipulator_control/ManipulatorService.hpp
+++ include/manipulator_control/ManipulatorService.hpp
@@ -24,12 +24,35 @@ public:
 
 private:
   bool callbackService(r2d2_msg_pkg::ManipulatorCommand::Request &req,
-                       r2d2_msg_pkg::ManipulatorCommand::Response &res);
+                       r2d2_msg_pkg::ManipulatorCommand::Response &res) {
+    res.success = true;
+    return callbackModeService(req, res) && callbackNozzleService(req, res) &&
+           callbackStatusService(req, res);
+  }
+
   bool callbackModeService(r2d2_msg_pkg::ManipulatorCommand::Request &req,
-                           r2d2_msg_pkg::ManipulatorCommand::Response &res);
+                           r2d2_msg_pkg::ManipulatorCommand::Response &res) {
+    ROS_DEBUG_STREAM(
+        "callbackModeService::got request, work_mode: " << req.work_mode);
+    if (!m_manipulatorControl->setMode(req.work_mode))
+      res.success &= false;
+    return true;
+  }
   bool callbackNozzleService(r2d2_msg_pkg::ManipulatorCommand::Request &req,
-                             r2d2_msg_pkg::ManipulatorCommand::Response &res);
+                             r2d2_msg_pkg::ManipulatorCommand::Response &res) {
+    ROS_DEBUG_STREAM("callback_nozzle_service::got request, nozzle_type: "
+                     << req.nozzle_type);
+    if (!m_manipulatorControl->setNozzle(req.nozzle_type))
+      res.success &= false;
+    return true;
+  }
   bool callbackStatusService(r2d2_msg_pkg::ManipulatorCommand::Request &req,
-                             r2d2_msg_pkg::ManipulatorCommand::Response &res);
+                             r2d2_msg_pkg::ManipulatorCommand::Response &res) {
+    ROS_DEBUG_STREAM("callback_status_service::got request, lock_status: "
+                     << req.lock_status);
+    if (!m_manipulatorControl->setLock(req.lock_status))
+      res.success &= false;
+    return true;
+  }
 };
 #endif // R2D2_MANIPULATOR_SERVICE_H
