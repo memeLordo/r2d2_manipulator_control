24f275b012bd36669ae98d877aa31306fd10edfa
memeLordo <serafim.kripachev@gmail.com>
2025-09-12 21:22:09 +0300

    ManipulatorControl: Переименоване m_params -> m_config
    Чтобы лучше отразить функионал (якобы) неизменяемой переменной
    конфигурации, схожей с оной у JointHandler.

--- include/manipulator_control/ManipulatorControl.hpp
+++ include/manipulator_control/ManipulatorControl.hpp
@@ -14,14 +14,14 @@ class ManipulatorConfig
   r2d2_state::WorkModePair m_workMode{};
   r2d2_state::NozzleTypePair m_nozzleType{};
   r2d2_state::LockStatusPair m_lockStatus{};
-  r2d2_type::callback::manipulator16_t<T> m_params;
+  r2d2_type::config::manipulator_t<T> m_config;
 
  protected:
   explicit ManipulatorConfig(const std::string &fileName = "manipulator")
       : IConfigJsonMap<r2d2_type::config::manipulator_t, T>{fileName} {};
 
  public:
-  void updateNozzleType() { m_params = this->getParams(m_nozzleType.key); };
+  void updateNozzleType() { m_config = this->getParams(m_nozzleType.key); };
   void resetMode() {
     ROS_DEBUG("Reset mode");
     m_workMode.type = r2d2_state::WorkMode::NONE;
@@ -69,7 +69,7 @@ class ManipulatorControlHandler : public ManipulatorConfig<T> {
   using ManipulatorConfig<T>::m_workMode;
   using ManipulatorConfig<T>::m_nozzleType;
   using ManipulatorConfig<T>::m_lockStatus;
-  using ManipulatorConfig<T>::m_params;
+  using ManipulatorConfig<T>::m_config;
   PayloadHandler<T> m_payload;
   PipeHandler<T> m_pipe;
   ElbowHandler<T> m_elbow;
@@ -107,21 +107,21 @@ class ManipulatorControlHandler : public ManipulatorConfig<T> {
   };
   void publishResults() {
     ROS_DEBUG_STREAM(MAGENTA("\npublishResults()"));
-    m_elbow.publish();
     m_shoulder.publish();
+    m_elbow.publish();
   };
 
  public:
   T getTargetForce() const {
-    const T force_{static_cast<T>(m_params.force_needed)};
+    const T force_{static_cast<T>(m_config.force_needed)};
     ROS_DEBUG_STREAM("ManipulatorControl::getForce() : " << WHITE(force_));
     return force_;
   };
   T getForceTolerance() const {
-    return static_cast<T>(m_params.force_tolerance);
+    return static_cast<T>(m_config.force_tolerance);
   };
   T getRadius() const {
-    const T radius_{m_params.r0};
+    const T radius_{m_config.r0};
     ROS_DEBUG_STREAM("ManipulatorControl::getRadius() : " << WHITE(radius_));
     return radius_;
   };
