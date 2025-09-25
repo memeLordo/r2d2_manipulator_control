95c42242a93807fcdb6ba78914df92c56ba5e3e3
memeLordo <serafim.kripachev@gmail.com>
2025-09-26 01:26:28 +0300

    Обновление Payload

--- include/manipulator_control/PayloadHandler.hpp
+++ include/manipulator_control/PayloadHandler.hpp
@@ -5,22 +5,31 @@
 
 #include "r2d2_msg_pkg/DriverState.h"
 #include "r2d2_utils_pkg/Debug.hpp"
+#include "r2d2_utils_pkg/Strings.hpp"
 #include "r2d2_utils_pkg/Types.hpp"
 
-template <typename T = double>
-class PayloadHandler {
- private:
-  static constexpr const char* s_name = "Payload";
-
- private:
+class PayloadConfig {
+ protected:
+  const std::string m_name;
   const std::string m_outputTopic;
+
+ protected:
+  explicit PayloadConfig(const std::string& name = "payload")
+      : m_name{r2d2_string::upper(name, 0, 1)},
+        m_outputTopic{"/" + name + "_output"} {};
+};
+
+template <typename T = double>
+class PayloadHandler : PayloadConfig {
+ private:
+  using PayloadConfig::m_name;
+  using PayloadConfig::m_outputTopic;
   r2d2_type::callback::payload16_t m_callbackParams{};
   ros::Subscriber m_subscriber;
 
  public:
   PayloadHandler() = default;
-  explicit PayloadHandler(ros::NodeHandle* node)
-      : m_outputTopic{"/payload_output"} {
+  explicit PayloadHandler(ros::NodeHandle* node) : PayloadConfig{} {
     waitForTopic();
     m_subscriber = node->subscribe(m_outputTopic, 1,
                                    &PayloadHandler::callbackPayload, this);
@@ -37,12 +46,12 @@ class PayloadHandler {
 
  public:
   void waitForTopic() {
-    ROS_INFO_STREAM(CYAN("Waiting for " << s_name << " topic..."));
+    ROS_INFO_STREAM(CYAN("Waiting for " << m_name << " topic..."));
     ros::topic::waitForMessage<r2d2_msg_pkg::DriverState>(m_outputTopic);
   };
   T getForce() const {
     const T force_{static_cast<T>(m_callbackParams.force)};
-    ROS_DEBUG_STREAM(s_name << "::getForce() : " << WHITE(force_));
+    ROS_DEBUG_STREAM(m_name << "::getForce() : " << WHITE(force_));
     return force_;
   };
 };
