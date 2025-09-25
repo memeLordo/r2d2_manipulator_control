f35f1652aff9075f346d0ea631399d600c82f889
memeLordo <serafim.kripachev@gmail.com>
2025-09-25 20:52:05 +0300

    fixup! Math: Переименование convertConfig -> json

--- src/Math.cpp
+++ src/Math.cpp
@@ -3,7 +3,7 @@
 #include "r2d2_utils_pkg/Json.hpp"
 
 namespace r2d2_process {
-IJsonConfig<> json{"convert_rate"};
-const double Angle::s_convertRatio{json.getParam("angle")};
-const double Force::s_convertRatio{json.getParam("force")};
+IJsonConfig<> jc{"convert_rate"};
+const double Angle::s_convertRatio{jc.getParam("angle")};
+const double Force::s_convertRatio{jc.getParam("force")};
 }  // namespace r2d2_process
