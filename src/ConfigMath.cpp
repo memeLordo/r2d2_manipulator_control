18d91d3d90af821daf426a49e9642404d644f8bf
memeLordo <serafim.kripachev@gmail.com>
2025-09-25 18:47:38 +0300

    ConfigMath: Обновление коэффициентов
    Замена местами деление/умножение при преобразовании значений из/в
    контроллера -> локальная переменная params.

--- src/ConfigMath.cpp
+++ src/ConfigMath.cpp
@@ -1,6 +1,6 @@
 #include "r2d2_utils_pkg/Math.hpp"
 
 namespace r2d2_process {
-const double Angle::s_convertRatio{-1e2};
-const double Force::s_convertRatio{5.88e6};
+const double Angle::s_convertRatio{-1e-2};
+const double Force::s_convertRatio{5.88e-4};
 }  // namespace r2d2_process
