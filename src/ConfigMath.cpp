71ea526a1509e29429b23876936ee0ef43b07f5f
memeLordo <serafim.kripachev@gmail.com>
2025-09-25 18:59:11 +0300

    ConfigMath: number -> json-config

--- src/ConfigMath.cpp
+++ src/ConfigMath.cpp
@@ -1,6 +1,8 @@
+#include "r2d2_utils_pkg/ConfigJson.hpp"
 #include "r2d2_utils_pkg/Math.hpp"
 
 namespace r2d2_process {
-const double Angle::s_convertRatio{-1e-2};
-const double Force::s_convertRatio{5.88e-4};
+IConfigJson<> convertConfig{"convert_rate"};
+const double Angle::s_convertRatio{convertConfig.getParam("angle")};
+const double Force::s_convertRatio{convertConfig.getParam("force")};
 }  // namespace r2d2_process
