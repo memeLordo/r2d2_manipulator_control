c14f6e615cae425f88ee5282da482838a293b818
memeLordo <serafim.kripachev@gmail.com>
2025-10-02 00:11:00 +0300

    main: Обновление наименований

--- src/manipulator_control_node.cpp
+++ src/manipulator_control_node.cpp
@@ -6,13 +6,13 @@ int main(int argc, char** argv) {
   ros::init(argc, argv, "manipulator_control");
   ros::NodeHandle node;
   try {
-    CHECK_FOR_ERRORS();
+    CHECK_FOR_STACK_ERRORS();
     TopicServiceHandler ts{&node};
     ManipulatorControlHandler mc{&node};
     ManipulatorServiceHandler ms{&node, mc};
     ROS_INFO("Manipulator Control started");
     ros::spin();
-  } catch (const r2d2_errors::ExceptionStack& e) {
+  } catch (const r2d2_errors::RuntimeErrorStack& e) {
     ROS_ERROR_STREAM("Got exception: " << GREEN(e.what()));
     PROCESS_ERROR_STACK();
   } catch (std::exception& e) {
