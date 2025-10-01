cc4b9d196072f975c2b9b17359e14e57bde83d16
memeLordo <serafim.kripachev@gmail.com>
2025-10-01 05:03:15 +0300

    main: Добавление обработки ошибок ErrorStack

--- src/manipulator_control_node.cpp
+++ src/manipulator_control_node.cpp
@@ -6,11 +6,15 @@ int main(int argc, char** argv) {
   ros::init(argc, argv, "manipulator_control");
   ros::NodeHandle node;
   try {
+    CHECK_FOR_ERRORS();
     TopicServiceHandler ts{&node};
     ManipulatorControlHandler mc{&node};
     ManipulatorServiceHandler ms{&node, mc};
     ROS_INFO("Manipulator Control started");
     ros::spin();
+  } catch (const r2d2_errors::ExceptionStack& e) {
+    ROS_ERROR_STREAM("Got exception: " << GREEN(e.what()));
+    PROCESS_ERROR_STACK();
   } catch (std::exception& e) {
     ROS_ERROR_STREAM("Got exception: " << e.what());
   }
